611dd3435d4178524437351b3195a0899cc3f4f2
maxde6de <63389675+maxde6de@users.noreply.github.com>
2025-05-21 09:27:25 +0200

    More todos added :)

--- scripts/Board/Game.js
+++ scripts/Board/Game.js
@@ -1,20 +1,15 @@
 
-
-
-// Found check
-//  1. call event when card is being flipped.
-//  2. check if there are now 2 cards flipped (this is possible by checking the length of 'selectedCards' in Cards (maybe create a function for this in Cards))
-//  3. check if their characters are the same.
-//  4. if characters are the same set found attribute (Cards.DATA_CARD_FOUND_ATR) of both cards to true attribute (Attributes.BOOLEAN_ATR_TRUE)
-//  5. update both characters and the background (boardFoundColor), this sould be done in Cards module.
-
-// Won check
+// todo: Won check
 //  1. check if all the cards are found
 //  2. if true then go to won state
 //  3. if false then resume game (do nothing)
 
-// Timer and progressbar
+// todo: elapsed time
 
-// character on board set
+// todo: remaining time and progressbar
 
-// board size set
+// todo: pairs found
+
+// todo: character on board set
+
+// todo: board size set
